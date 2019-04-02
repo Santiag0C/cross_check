efb2e923d48e98b84b9ccbbf23be6fd9f363531b
lpile <logan.pile@gmail.com>
2019-04-01 22:18:16 -0600

    add to StatTracker class to instantiate Team class. Also deleted dummy_game.csv file

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require './lib/game'
+require './lib/team'
 
 class StatTracker
   attr_reader :games,
@@ -8,11 +9,13 @@ class StatTracker
 
   def initialize(data)
     @games = data[:games].map {|game| Game.new(game.to_hash)}
+    @teams = data[:teams].map {|team| Team.new(team.to_hash)}
   end
 
   def self.from_csv(files)
     StatTracker.new({
-            games: CSV.open(files[:games], headers: true)
+            games: CSV.open(files[:games], headers: true),
+            teams: CSV.open(files[:teams], headers: true)
             })
   end
 end
