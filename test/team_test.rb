17818d9ead95acd04bab1b6f8c2795a5cd1c2bf9
lpile <logan.pile@gmail.com>
2019-04-01 22:12:54 -0600

    built Team class and team_test files

---
+++ test/team_test.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/team'
+
+class TeamTest < Minitest::Test
+  def setup
+    @team_data = {
+                "team_id" => "1",
+                "franchiseId" => "23",
+                "shortName" => "New Jersey",
+                "teamName" => "Devils",
+                "abbreviation" => "NJD",
+                "link" => "/api/v1/teams/1"}
+
+    @team = Team.new(@team_data)
+  end
+
+  def test_team_class_exist
+    assert_instance_of Team, @team
+  end
+
+  def test_team_class_has_attributes
+    assert_equal "1", @team.team_id
+    assert_equal "23", @team.franchiseId
+    assert_equal "New Jersey", @team.shortName
+    assert_equal "Devils", @team.teamName
+    assert_equal "NJD", @team.abbreviation
+    assert_equal "/api/v1/teams/1", @team.link
+  end
+end
