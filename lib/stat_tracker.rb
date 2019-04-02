6a87e28d4cdb8a73a930e4ca3b99cd943511fcfd
lpile <logan.pile@gmail.com>
2019-04-01 22:05:11 -0600

    built StatTracker class initialize and from_csv method to accept dummy_game.csv which instantiate game objects into hash

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,15 +1,18 @@
 require 'csv'
+require './lib/game'
 
 class StatTracker
   attr_reader :games,
               :teams,
               :game_teams
 
-  def initialize
-
+  def initialize(data)
+    @games = data[:games].map {|game| Game.new(game.to_hash)}
   end
 
   def self.from_csv(files)
-
+    StatTracker.new({
+            games: CSV.open(files[:games], headers: true)
+            })
   end
 end
