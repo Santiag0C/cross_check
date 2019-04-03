829a7061ab9b9ce1b5eb81c693d78b48274f1f83
Santiago Caraveo <santiagocbrvg@hotmail.com>
2019-04-02 21:47:04 -0700

    adds test for the biggest blowout method

--- test/game_statistic_test.rb
+++ test/game_statistic_test.rb
@@ -6,12 +6,23 @@ require './lib/stat_tracker'
 require 'pry'
 
 
-class GameStatisticsTest < Minitest::Test
-  def test_highest_total_score
+class StatTrackerTest < Minitest::Test
+  def setup
     game_path = './data/dummy_game.csv'
-    locations = {games: game_path}
-    binding.pry
-    stat_tracker = StatTracker.from_csv(locations[:games])
-    assert_equal 6, stat_tracker.highest_total_score
+    team_path = './data/team_info.csv'
+    game_teams_path = './data/dummy_game_teams.csv'
+
+    @locations = {games: game_path,
+                 teams: team_path,
+                 game_teams: game_teams_path}
+  end
+  def test_highest_and_lowest_total_score
+    stat_tracker = StatTracker.from_csv(@locations)
+    assert_equal 7, stat_tracker.highest_total_score
+    assert_equal 3, stat_tracker.lowest_total_score
+  end
+  def test_biggest_blowout
+    stat_tracker = StatTracker.from_csv(@locations)
+    assert_equal 3, stat_tracker.biggest_blowout
   end
 end
