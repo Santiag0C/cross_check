65fe6fcf1ba1b382a8817c47f6ea4fdf3aaba22e
Santiago Caraveo <santiagocbrvg@hotmail.com>
2019-04-03 09:20:40 -0700

    adds percentage_home_wins

--- lib/game_statistics_module.rb
+++ lib/game_statistics_module.rb
@@ -20,6 +20,38 @@ module GameStatistics
     difference << (goals[1] - goals[0])
     goals.pop(2)
       end
+      binding.pry
   difference.max
+  end
+
+  def percentage_home_wins
+    games_won = 0
+    @games.each do |game|
+      if game.home_goals > game.away_goals
+        games_won += 1
+      end
+    end
+    percentage = (games_won.to_f/@games.count) * 100
+    percentage.round(2)
+  end
+
+  def percentage_visitor_wins
+    100.00 - percentage_home_wins
+  end
+
+  def count_of_games_by_season
+    hash = {}
+    @games.each do |game|
+      if hash.key?(game.season) == false
+        hash[game.season] = 1
+      elsif hash.key?(game.season) == true
+        hash[game.season] += 1
+      end
+    end
+    hash
+  end
+  
+  def average_goals_per_game
+
   end
 end
