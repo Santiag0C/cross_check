c0dc8690e3ca4fe4480fd1ca7561038317540392
James Cape <james.cape@gmail.com>
2019-04-03 09:19:37 -0600

    Complete % home wins, % visitor wins, and count of games by season

--- test/game_statistic_test.rb
+++ test/game_statistic_test.rb
@@ -22,4 +22,59 @@ class GameStatisticsTest < Minitest::Test
 
     assert_equal 6, stat_tracker.highest_total_score
   end
+
+
+
+
+
+
+  def test_percentage_home_wins
+    game_path = './data/dummy_game.csv'
+    team_path = './data/dummy_teams.csv'
+    game_teams_path = './data/dummy_game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    stat_tracker = StatTracker.from_csv(locations)
+
+    assert_equal 0.67, stat_tracker.percentage_home_wins
+  end
+
+  def test_percentage_visitor_wins
+    game_path = './data/dummy_game.csv'
+    team_path = './data/dummy_teams.csv'
+    game_teams_path = './data/dummy_game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    stat_tracker = StatTracker.from_csv(locations)
+
+    assert_equal 0.33, stat_tracker.percentage_visitor_wins
+  end
+
+  def test_count_of_games_by_season
+    game_path = './data/dummy_game.csv'
+    team_path = './data/dummy_teams.csv'
+    game_teams_path = './data/dummy_game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    stat_tracker = StatTracker.from_csv(locations)
+    expected = {"20122013"=>3}
+    assert_equal expected, stat_tracker.count_of_games_by_season
+  end
+
+
 end
