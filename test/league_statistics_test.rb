000358e5b37db754530da1e6cfca1bb1b2029427
lpile <logan.pile@gmail.com>
2019-04-04 08:41:42 -0600

    "adds winningest_team, best_fans, and worst_fans in league_statistics_module and also their tests"

--- test/league_statistics_test.rb
+++ test/league_statistics_test.rb
@@ -19,18 +19,25 @@ class LeagueStatisticsTest < Minitest::Test
     @stat_tracker = StatTracker.from_csv(@locations)
   end
 
-  def test_count_of_teams_in_league
-    assert_equal 32, @stat_tracker.count_of_teams
+  def test_group_by_teams
+    assert_equal 7, @stat_tracker.group_by_teams.length
   end
 
-  def test_best_offense_in_league
-    assert_equal 1, @stat_tracker.best_offense
+  def test_return_team_name
+    assert_equal "Devils", @stat_tracker.return_team_name('1')
   end
 
-  def test_worst_offense_in_league
+  def test_winningest_team
+    assert_equal "Senators", @stat_tracker.winningest_team
+  end
+
+  def test_best_fans
     skip
-    assert_equal 1, @stat_tracker.worst_offense
+    # Need to put in more data in dummy file
+    assert_equal 1, @stat_tracker.best_fans
   end
 
-
+  def test_worst_fans
+    assert_equal [], @stat_tracker.worst_fans
+  end
 end
