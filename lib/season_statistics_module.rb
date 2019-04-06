2006a59906576df4f75edb0087f1d22eaf82ac13
lpile <logan.pile@gmail.com>
2019-04-06 07:10:35 -0600

    'add skeleton season_statistics_module.rb and test'

---
+++ lib/season_statistics_module.rb
@@ -0,0 +1,47 @@
+module SeasonStatistics
+
+  def biggest_bust
+    #Name of the team with the biggest decrease between regular season and postseason win percentage.
+    #return string
+  end
+
+  def biggest_surprise
+    #Name of the team with the biggest increase between regular season and postseason win percentage.
+    #return String
+  end
+
+  def winningest_coach
+    #Name of the Coach with the best win percentage for the season
+    #return String
+  end
+
+  def worst_coach
+    #Name of the Coach with the worst win percentage for the season
+    #return String
+  end
+
+  def most_accurate_team
+    #Name of the Team with the best ratio of shots to goals for the season
+    #return String
+  end
+
+  def least_accurate_team
+    #Name of the Team with the worst ratio of shots to goals for the season
+    #return String
+  end
+
+  def most_hits
+    #Name of the Team with the most hits in the season
+    #return String
+  end
+
+  def fewest_hits
+    #Name of the Team with the fewest hits in the season
+    #return String
+  end
+
+  def power_play_goal_percentage
+    #Percentage of goals that were power play goals for the season (rounded to the nearest 100th)
+    #return Float
+  end
+end
---
+++ test/season_statistics_test.rb
@@ -0,0 +1,56 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/stat_tracker'
+require 'pry'
+
+class SeasonStatisticsTest < Minitest::Test
+  def setup
+    game_path = './data/dummy_game.csv'
+    team_path = './data/team_info.csv'
+    game_teams_path = './data/dummy_game_teams.csv'
+
+    @locations = {games: game_path,
+                 teams: team_path,
+                 game_teams: game_teams_path}
+
+    @stat_tracker = StatTracker.from_csv(@locations)
+  end
+
+  def test_biggest_bust
+
+  end
+
+  def test_biggest_surprise
+
+  end
+
+  def test_winningest_coach
+
+  end
+
+  def test_worst_coach
+
+  end
+
+  def test_most_accurate_team
+
+  end
+
+  def test_least_accurate_team
+
+  end
+
+  def test_most_hits
+
+  end
+
+  def test_fewest_hits
+
+  end
+
+  def test_power_play_goal_percentage
+
+  end
+end
