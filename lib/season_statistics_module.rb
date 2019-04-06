ddfe170dbf622171e42a5da77b996b3f4b624d36
lpile <logan.pile@gmail.com>
2019-04-06 07:15:56 -0600

    'update skeleton season_statistics_module'

--- lib/season_statistics_module.rb
+++ lib/season_statistics_module.rb
@@ -1,47 +1,47 @@
 module SeasonStatistics
 
-  def biggest_bust
+  def biggest_bust(season_id)
     #Name of the team with the biggest decrease between regular season and postseason win percentage.
-    #return string
+    #return team name string
   end
 
-  def biggest_surprise
+  def biggest_surprise(season_id)
     #Name of the team with the biggest increase between regular season and postseason win percentage.
-    #return String
+    #return team name string
   end
 
-  def winningest_coach
+  def winningest_coach(season_id)
     #Name of the Coach with the best win percentage for the season
-    #return String
+    #return coach name string
   end
 
-  def worst_coach
+  def worst_coach(season_id)
     #Name of the Coach with the worst win percentage for the season
-    #return String
+    #return coach name string
   end
 
-  def most_accurate_team
+  def most_accurate_team(season_id)
     #Name of the Team with the best ratio of shots to goals for the season
-    #return String
+    #return team name string
   end
 
-  def least_accurate_team
+  def least_accurate_team(season_id)
     #Name of the Team with the worst ratio of shots to goals for the season
-    #return String
+    #return team name string
   end
 
-  def most_hits
+  def most_hits(season_id)
     #Name of the Team with the most hits in the season
-    #return String
+    #return team name string
   end
 
-  def fewest_hits
+  def fewest_hits(season_id)
     #Name of the Team with the fewest hits in the season
-    #return String
+    #return team name string
   end
 
-  def power_play_goal_percentage
+  def power_play_goal_percentage(season_id)
     #Percentage of goals that were power play goals for the season (rounded to the nearest 100th)
-    #return Float
+    #return percentage float
   end
 end
