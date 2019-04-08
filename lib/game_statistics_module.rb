f4d0e7d9e64bd5a8d0189a994093b6097b920f12
lpile <logan.pile@gmail.com>
2019-04-08 17:12:48 -0600

    delete @ from ivar in module

--- lib/game_statistics_module.rb
+++ lib/game_statistics_module.rb
@@ -1,33 +1,33 @@
 module GameStatistics
   def highest_total_score
-    high_game = @games.max_by {|game| game.away_goals + game.home_goals}
+    high_game = games.max_by {|game| game.away_goals + game.home_goals}
     high_game.away_goals + high_game.home_goals
   end
 
   def lowest_total_score
-    low_game = @games.min_by {|game| game.away_goals + game.home_goals}
+    low_game = games.min_by {|game| game.away_goals + game.home_goals}
     low_game.away_goals + low_game.home_goals
   end
 
   def biggest_blowout
-    difference = @games.max_by{|game| (game.away_goals - game.home_goals).abs}
+    difference = games.max_by{|game| (game.away_goals - game.home_goals).abs}
     (difference.away_goals - difference.home_goals).abs
   end
 
   def percentage_home_wins
-    home_wins = @games.count { |game| game.away_goals < game.home_goals}
-    (home_wins.to_f / @games.length).round(2)
+    home_wins = games.count { |game| game.away_goals < game.home_goals}
+    (home_wins.to_f / games.length).round(2)
   end
 
   def percentage_visitor_wins
-    visitor_wins = @games.count { |game| game.away_goals > game.home_goals}
-    (visitor_wins.to_f / @games.length).round(2)
+    visitor_wins = games.count { |game| game.away_goals > game.home_goals}
+    (visitor_wins.to_f / games.length).round(2)
   end
 
   def count_of_games_by_season
     games_by_season = {}
-    @games.each do |game|
-      games_by_season[game.season] = @games.count { |i| game.season == i.season}
+    games.each do |game|
+      games_by_season[game.season] = games.count { |i| game.season == i.season}
     end
     games_by_season
   end
@@ -35,15 +35,15 @@ module GameStatistics
   def average_goals_per_game
     total_goals = 0
     total_games = []
-    @games.map {|game| total_goals += game.away_goals + game.home_goals}
-    @games.map {|game| total_games << game.game_id}
+    games.map {|game| total_goals += game.away_goals + game.home_goals}
+    games.map {|game| total_games << game.game_id}
     (total_goals.to_f / total_games.length).round(2)
   end
 
   def average_goals_by_season
     goals_by_season = {}
-    @games.each do |game|
-      goals_by_season[game.season] = @games.map {|i| i.away_goals + i.home_goals if game.season == i.season}.compact.sum
+    games.each do |game|
+      goals_by_season[game.season] = games.map {|i| i.away_goals + i.home_goals if game.season == i.season}.compact.sum
     end
     array = [goals_by_season, count_of_games_by_season]
     keys = goals_by_season.keys
