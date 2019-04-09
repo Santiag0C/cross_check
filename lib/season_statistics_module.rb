4d9dbeb4ae38c66ced30490fdb43bd42abfee2a6
Santiago Caraveo <santiagocbrvg@hotmail.com>
2019-04-09 13:46:22 -0700

    makes fewer hits faster, most hits fater and pore organized

--- lib/season_statistics_module.rb
+++ lib/season_statistics_module.rb
@@ -1,15 +1,17 @@
+require 'pry'
 module SeasonStatistics
 
-
-  def season_gather #helper
-    seasons =[]
-      @games.each do |game|
-        seasons << game.season
-      end
-    seasons.uniq
+  def most_hits(season)
+    highest = hit_helper(season).invert.max
+    name_finder(highest)
   end
 
-  def hit_helper(season)
+  def fewest_hits(season)
+    fewest = hit_helper(season).invert.min
+    name_finder(fewest)
+  end
+
+  def hit_helper(season)#helper
     hash = Hash.new{|h,k| h[k] = [] }
      season_gather.each do |season|
       @games.each do |game|
@@ -29,24 +31,19 @@ module SeasonStatistics
   team_hits
   end
 
-  def most_hits(season)
-      highest = hit_helper(season).invert.max
-      name_finder(highest)
-  end
-
   def name_finder(something) #helper
     @teams.each do |team|
-
-
-    end
-  end
-
-  def fewest_hits(season)
-    @teams.each do |team|
-      highest = hit_helper(season).invert.min
-      if team.team_id == highest[1]
+      if team.team_id == something[1]
         return team.team_name
       end
-    endgi
+  end
+  end
+
+  def season_gather #helper
+    seasons =[]
+    @games.each do |game|
+      seasons << game.season
+    end
+    seasons.uniq
   end
 end
